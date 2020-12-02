74e4b8ee216e34fe8e01b157b095f2ab49204d2c
Shoaib <shoaib.ahmed@slock.it>
2020-12-03 00:55:06 +0530

    Major refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -17,13 +17,40 @@ struct Signature {
 }
 
 impl Signature {
+    #[inline]
     fn new(chunk_sz: usize) -> Self {
         Signature { chunk_sz, hashes: vec![] }
+    }
+
+    fn from_buffer(data: &[u8], chunk_sz: usize) -> Result<Signature, Error> {
+        if data.len() < chunk_sz {
+            return Err(SignatureError::BadChunkSize.into());
+        }
+
+        let mut sig = Signature::new(chunk_sz);
+        let chunks = data.chunks(chunk_sz).collect::<Vec<&[u8]>>();
+        for (id, &chunk) in chunks.iter().enumerate() {
+            let strong = md5::compute(chunk);
+            let weak = adler32::adler32(chunk).expect("reading from chunk cannot fail");
+            sig.hashes.push(ChunkHash { offset: chunk_sz * id, strong, weak });
+        }
+
+        Ok(sig)
+    }
+
+    #[inline]
+    fn find(&self, weak_hash: WeakHash) -> Option<&ChunkHash> {
+        for chash in &self.hashes {
+            if chash.weak == weak_hash {
+                return Some(&chash);
+            }
+        }
+        None
     }
 }
 
 #[derive(Debug)]
-enum Ops<'a> {
+enum DeltaType<'a> {
     Chunk(&'a ChunkHash),
     Raw { offset: usize, data: &'a [u8] },
 }
@@ -31,7 +58,38 @@ enum Ops<'a> {
 #[derive(Debug)]
 struct Delta<'a> {
     full_checksum: StrongHash,
-    ops: Vec<Ops<'a>>,
+    ops: Vec<DeltaType<'a>>,
+}
+
+impl<'a> Delta<'a> {
+    #[inline]
+    fn add_raw(&mut self, data: &'a [u8], last_match_end: Option<usize>, pos: usize) {
+        if let Some(last_match_end) = last_match_end {
+            if last_match_end != pos {
+                self.ops.push(DeltaType::Raw { offset: last_match_end, data: &data[last_match_end..pos] });
+            }
+        }
+    }
+
+    fn from(data: &'a [u8], signature: &'a Signature) -> Delta<'a> {
+        let window = signature.chunk_sz;
+        let mut delta = Delta { full_checksum: md5::compute(data), ops: vec![] };
+        let mut last_match_end: Option<usize> = None;
+
+        let rh_itr = RollingHashItr::new(data, window);
+        for (pos, rh) in rh_itr {
+            if let Some(chash) = signature.find(rh) {
+                if chash.strong == md5::compute(&data[pos..(pos + window)]) {
+                    delta.add_raw(data, last_match_end, pos);
+                    delta.ops.push(DeltaType::Chunk(chash));
+                    last_match_end = Some(pos + window);
+                }
+            }
+        }
+
+        delta.add_raw(data, last_match_end, data.len());
+        delta
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -49,61 +107,6 @@ impl convert::From<SignatureError> for Error {
     fn from(err: SignatureError) -> Self {
         Self::SignatureError(err)
     }
-}
-
-fn get_signature(data: &[u8], chunk_sz: usize) -> Result<Signature, Error> {
-    if data.len() < chunk_sz {
-        return Err(SignatureError::BadChunkSize.into());
-    }
-
-    let mut sig = Signature::new(chunk_sz);
-    let chunks = data.chunks(chunk_sz).collect::<Vec<&[u8]>>();
-    for (id, &chunk) in chunks.iter().enumerate() {
-        let strong = md5::compute(chunk);
-        let weak = adler32::adler32(chunk).expect("reading from chunk cannot fail");
-        sig.hashes.push(ChunkHash { offset: chunk_sz * id, strong, weak });
-    }
-
-    Ok(sig)
-}
-
-fn matching_chunk_hash(hashes: &[ChunkHash], weak_hash: WeakHash) -> Option<&ChunkHash> {
-    for hash in hashes {
-        if hash.weak == weak_hash {
-            return Some(&hash);
-        }
-    }
-    None
-}
-
-fn add_remaining_raw<'a>(mut delta: Delta<'a>, data: &'a [u8], last_match_end: Option<usize>, pos: usize) -> Delta<'a> {
-    if let Some(last_match_end) = last_match_end {
-        if last_match_end != pos {
-            delta.ops.push(Ops::Raw { offset: last_match_end, data: &data[last_match_end..pos] });
-        }
-    }
-    delta
-}
-
-fn get_delta<'a>(data: &'a [u8], signature: &'a Signature) -> Delta<'a> {
-    let window = signature.chunk_sz;
-    let mut delta = Delta { full_checksum: md5::compute(data), ops: vec![] };
-    let rh_itr = RollingHashItr::new(data, window);
-    let mut last_match_end: Option<usize> = None;
-
-    for (i, rh) in rh_itr {
-        if let Some(chash) = matching_chunk_hash(&signature.hashes, rh) {
-            let sh = md5::compute(&data[i..(i + window)]);
-            if sh == chash.strong {
-                delta = add_remaining_raw(delta, data, last_match_end, i);
-                delta.ops.push(Ops::Chunk(chash));
-                last_match_end = Some(i + window);
-            }
-        }
-    }
-
-    delta = add_remaining_raw(delta, data, last_match_end, data.len());
-    delta
 }
 
 struct RollingHashItr<'a> {
@@ -142,22 +145,22 @@ impl Iterator for RollingHashItr<'_> {
 #[cfg(test)]
 mod tests {
     use std::io;
-    use crate::{get_signature, Error, get_delta};
+    use crate::{Error, Signature, Delta};
     use crate::SignatureError::BadChunkSize;
 
     #[test]
     fn test_signature() {
-        let sig = get_signature(b"abcdefg", 2);
+        let sig = Signature::from_buffer(b"abcdefg", 2);
         println!("{:?}", sig);
         assert!(sig.is_ok());
-        assert_eq!(get_signature(b"abcdefg", 8).err().unwrap(), Error::SignatureError(BadChunkSize))
+        assert_eq!(Signature::from_buffer(b"abcdefg", 8).err().unwrap(), Error::SignatureError(BadChunkSize))
     }
 
     #[test]
     fn test_delta() {
-        let sig = get_signature(b"abcdefgh", 2).unwrap();
+        let sig = Signature::from_buffer(b"abcdefgh", 2).unwrap();
         println!("{:?}", sig);
-        let delta = get_delta(b"abtkcdefgh", &sig);
+        let delta = Delta::from(b"abtkcdefgh", &sig);
         println!("{:?}", delta);
     }
 
